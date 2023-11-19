0f8e05ddc4b160d0e1b575ce3b790f7e12eccd74
Sakhile Mnguni <srb.mnguni@gmail.com>
2023-11-19 11:05:55 -0800

    19/11/2023

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import math.QuadraticEquestion;
+
+public class Main {
+    public static void main(String[] args) {
+        QuadraticEquestion qe1 = new QuadraticEquestion(1, 3, -4);
+
+        qe1.solveX();
+    }
+}
---
+++ src/math/QuadraticEquestion.java
@@ -0,0 +1,29 @@
+package math;
+
+public class QuadraticEquestion {
+    //ax^2 + bx + c
+    private final double a;
+    private final double b;
+    private final double c;
+
+    public QuadraticEquestion(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void solveX() {
+        System.out.println("x1 = 1\nx2 = -4");
+    }
+
+    public void dirivative() {
+
+    }
+
+    public void integrate() {
+
+    }
+
+
+
+}
